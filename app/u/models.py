61a8e764690a9246a126812ef992e19ce2a4b232
michael <michael.williams705@live.com>
2019-05-16 02:44:50 -0400

    add user database

--- app/u/models.py
+++ app/u/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    first_name = models.CharField(max_length=120)
+    last_name = models.CharField(max_length=120)
+    school_id = models.IntegerField()
+    school_email = models.EmailField()
+    is_teacher = models.BooleanField()
